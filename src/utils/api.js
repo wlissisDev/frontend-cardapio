2179160956e78e315d30f4a9554e20948d2d840b
wlissis <wlissisdev@gmail.com>
2023-10-03 10:03:13 -0300

    final commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const api = axios.create(
     { 
-        baseURL: "http://localhost:5000/" 
+        baseURL: "https://backend-cardapio.onrender.com/" 
     }
 )
